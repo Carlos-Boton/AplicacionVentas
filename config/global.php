03d1c399723b15e9c29de3e19ca8b8023b4295c9
Carlos Daniel <Carlos@Developer.com>
2025-01-31 22:36:28 -0600

    configuracion a la base de datos

--- config/global.php
+++ config/global.php
@@ -1,16 +1,18 @@
 <?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
+define("DB_HOST", "34.170.3.102");
+
+define("DB_PORT", "3306");
 
 // nombre de la base de datos
-define("DB_NAME", "crud");
+define("DB_NAME", "Distribuidor");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "Tidis");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "Cdsp-0317fbyG");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
