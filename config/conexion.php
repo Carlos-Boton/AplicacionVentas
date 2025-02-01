b0560b7b27152ef963f0344621894fe55210e0ed
Carlos Daniel <Carlos@Developer.com>
2025-01-31 23:29:23 -0600

    base de datos

--- config/conexion.php
+++ config/conexion.php
@@ -1,40 +1,22 @@
-<?php 
-require_once "global.php";
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$conexion=new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$host = '34.170.3.102'; // Dirección IP pública de Google Cloud SQL
+$db = 'Distribuidor';
+$user = 'Tidis';
+$pass = 'Cdsp-0317fbyG';
 
-mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
-
-//muestra posible error en la conexion
-if (mysqli_connect_errno()) {
-	printf("Falló en la conexion con la base de datos: %s\n",mysqli_connect_error());
-	exit();
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+try {
+    $options = array(
+        PDO::ATTR_TIMEOUT => 10, // Establece el tiempo de espera en segundos
+    );
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Conexión exitosa!";
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
 }
 
-if (!function_exists('ejecutarConsulta')) {
-	Function ejecutarConsulta($sql){ 
-    global $conexion;
-    $query=$conexion->query($sql);
-    return $query;
-
-        }
-
-        function ejecutarConsultaSimpleFila($sql){
-    global $conexion;
-    $query=$conexion->query($sql);
-    $row=$query->fetch_assoc();
-    return $row;
-        }
-    function ejecutarConsulta_retornarID($sql){
-    global $conexion;
-    $query=$conexion->query($sql);
-    return $conexion->insert_id;
-    }
-
-    function limpiarCadena($str){
-    global $conexion;
-    $str=mysqli_real_escape_string($conexion,trim($str));
-    return htmlspecialchars($str);
-    }
-
-}
+?>
